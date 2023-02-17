2f07f35fb933d822c45672aa5aab0eb1e27b1904
LN <62269693+ln-dev7@users.noreply.github.com>
2023-02-17 10:19:48 +0100

    update website link

--- src/App.svelte
+++ src/App.svelte
@@ -105,7 +105,7 @@
           </div>
           <div class="card-other">
             {#if blog}
-              <a href={""+blog} class="card-other__item">
+              <a href={" "+blog} class="card-other__item">
                 <svg
                   width="35"
                   height="35"
